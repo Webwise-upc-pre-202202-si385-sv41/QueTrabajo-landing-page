de1a3cf82114272128dea466c4932aa625f9d0d0
Lucía Aliaga <91093842+LuciaAliagaT@users.noreply.github.com>
2022-11-25 18:29:25 -0500

    fixed navbar

---
+++ public/public/assets/scripts/function.js
@@ -0,0 +1,5 @@
+document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+    document.querySelector(".container-menu").classList.toggle("mostrar_menu");
+}
